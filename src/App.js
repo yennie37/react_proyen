6b9d5c547164d412d63b07545f46a007d8d33427
Yeon <yennie37@naver.com>
2023-01-22 22:03:20 +0900

    [버전이슈] Home의 path를 변경

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       {/** Switch가 하는 일 ? Route를 찾아서 컴포넌트를 랜더링. */}
       {/** Switch => 버전6부터 Routes로 바뀜 */}
       <Routes>
-        <Route path="/" element={<Home/>}/>          
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>          
         {/** ':'를 빼놓지말고 꼭 써야함! 안그럼 단순 string으로 id가 출력됨 */}
         <Route path="/movie/:id" element={<Detail/>}/>
         <Route path="/about-us"element={<h1>Hello</h1>}/>
