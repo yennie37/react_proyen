5e5429af8d273ea8a787c5caf21f19d688e1e55d
Yeon <yennie37@naver.com>
2023-02-14 22:44:07 +0900

    #2.2 useClick 오류 해결

--- src/routes/Hooks10_2.js
+++ src/routes/Hooks10_2.js
@@ -5,36 +5,40 @@ import styles from "./Hooks10.module.css";
 // 2-1
 const useTitle = (initialTitle) => {
   const [title, setTitle] = useState(initialTitle);
+
   const updateTitle = () => {
     const htmlTitle = document.querySelector("title");
     htmlTitle.innerHTML = title;
   }
+
   useEffect(updateTitle, [title]);
   return setTitle;
 };
 
 // 2-2
-// <h3 ref={title}>누르면 인사를 해요~! </h3>  의 클릭이벤트가 안먹는이유가 뭐야?
+// 터미널에 warning 뜨는건 왜뜨는걸까
 const useClick = (onClick) => {  
-  console.log("typeof onClick >> " + typeof onClick);
-  const ref = useRef();
-  
-  useEffect(() => {
-    const element = ref.current;
+  const element = useRef();
 
+  useEffect(() => {
     // componentDidMount 일때 동작(mount될때)
-    if(element) {
-      element.addEventListener("click", onClick);
+    if(element.current) {
+      element.current.addEventListener("click", onClick);
     } 
 
     // componentWillUnMount가 될때 addEventListener를 지워주기(mount되지 않을때)
     return () => {
-      if(element) {
-        element.removeEventListener("click", onClick);
+      if(element.current) {
+        element.current.removeEventListener("click", onClick);
       }  
     };
   }, [onClick]);
-  return ref.current;
+
+  if(typeof onClick !== "function") {
+    return;
+  }
+
+  return element;
 };
 
 function Hooks10_2() {  
@@ -51,7 +55,6 @@ function Hooks10_2() {
   // 2-2
   // reference 는 기본적으로 component의 어떤 부분을 선택할 수 있는 방법
   const tmpRef = useRef();
-  //setTimeout(() => console.log(tmpRef.current), 4000);
   setTimeout(() => tmpRef.current.focus(), 4000);
 
   const title = useClick(sayHello);
@@ -59,7 +62,7 @@ function Hooks10_2() {
   return (    
     <div>
       <h1 className={styles.title}>Hooks 테스트 페이지 2</h1>      
-        
+      
       <div>
         <h2>2-0. useEffect</h2>               
         <button onClick={() => setNumber(number + 1)}>{number}</button>
@@ -73,13 +76,12 @@ function Hooks10_2() {
         <h2>2-2. useClick - What is Reference ? </h2>         
         <input ref={tmpRef} placeholder="lalala" />
       </div>
-      
+        
       <div>
         <h2>2-2. useClick</h2>                     
-        {/* 왜 클릭이벤트가 안먹힐까.... */}
+        {/* warning 수정하려다가, 클릭이벤트가 안먹게되서 우선은 패스하고 Hooks 배우는데 집중하기로 함. */}
         <h3 ref={title}>누르면 인사를 해요~! </h3>          
       </div>
-
     </div>
   );
 }
