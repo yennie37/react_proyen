750d7ad70c90564fc74ab2c4f07ba58f992be220
Yeon <yennie37@naver.com>
2023-03-16 22:13:04 +0900

    1. Helmet 설치 2. title, favicon 변경

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 /** App component 전체를 가지고 있는 홈 라우트 */
 import {useNavigate} from "react-router-dom"; 
+import { Helmet } from 'react-helmet';
 import styles from "./Home.module.css";
 
 function Home() {  
@@ -7,6 +8,10 @@ function Home() {
   
   return (
     <div>
+      <Helmet>        
+        <title>연리둥절</title>
+      </Helmet>
+      
       <h1 className={styles.mainTitle}>Menu</h1> 
   
       <div className={styles.buttons}>        
